744b2baabebdc2883bb4969505015df263f502e0
han0won <han00won@gmail.com>
2024-03-04 18:17:16 +0900

    메인컨텐츠 채워넣는 중

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,43 +1,23 @@
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-
-let hotelsSection = document.querySelector('#hotels'),
-    hotelsContainer = document.querySelector('#hotels-wrap'),
-    tween;
-document.querySelectorAll('.anchor').forEach((anchor) => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        let targetElem = document.querySelector(e.target.getAttribute('href')),
-            y = targetElem;
-        if (targetElem && hotelsContainer.isSameNode(targetElem.parentElement)) {
-            let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
-                totalMovement = (hotels.length - 1) * targetElem.offsetWidth;
-            y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
-        }
-        gsap.to(window, {
-            scrollTo: {
-                y: y,
-                autoKill: false,
-            },
-            duration: 1,
-        });
-    });
-});
-
-
-const hotels = gsap.utils.toArray('#hotels-wrap .hotel');
-tween = gsap.to(hotels, {
-    xPercent: -100 * (hotels.length - 1),
-    ease: 'none',
-    scrollTrigger: {
-        trigger: '#hotels-wrap',
-        pin: true,
-        start: 'top top',
-        scrub: 1,
-        snap: {
-            snapTo: 1 / (hotels.length - 1),
-            inertia: false,
-            duration: { min: 0.1, max: 0.1 },
-        },
-        end: () => '+=' + (hotelsContainer.offsetWidth - innerWidth),
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
 });
+
+  
+{
+    "devDependencies": {
+        "serve": "^11.2.0"
+    },
+    "main": "index.html",
+    "name": "swiper-vertical",
+    "scripts": {
+        "build": "echo This is a static template, there is no bundler or bundling involved!",
+        "start": "serve"
+    },
+    "tags": [
+        "swiper"
+    ]
+}
